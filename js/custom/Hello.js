0b06ec19a29756cca3134f8604c1dad53c0baf6b
boudesit <boudesit@gmail.com>
2016-10-05 17:35:36 +0200

    fefe

--- js/custom/Hello.js
+++ js/custom/Hello.js
@@ -1,15 +1,10 @@
 //dojo_basedeclare and dijitDialog are loaded from CDN
-define([ "dojo/_base/declare", "dijit/Dialog", "custom/customButton" ], function(declare, Dialog, customButton){
+define([ "dojo/_base/declare", "dijit/Dialog" ], function(declare, Dialog){
 //  declare our custom class
 
-var b = new customButton(
-    onClick: function(){
-        // Do something:
-        dom.byId("greeting").innerHTML += " Thank you! ";
-    }, "progButtonNode");
 
    return dojo.declare(dijit.Dialog, {
-     title: "Hi",
-     content: b
+     title: "Hi"
+
    });
 });
--- js/custom/customButton.js
+++
@@ -1,6 +0,0 @@
-define([ "dojo/_base/declare", "dijit/form/Button"], function(declare, Button){
-    ready(function(){
-        return dojo.declare(dijit.form.Button, {
-          label: "Click me!"
-        });
-    });
