3f67f2715b75041c8d0a53cc2c3bba3b4ac2df00
boudesit <boudesit@gmail.com>
2016-10-05 16:57:23 +0200

    yEAHe ?

--- js/custom/Hello.js
+++ js/custom/Hello.js
@@ -1,5 +1,5 @@
 //dojo_basedeclare and dijitDialog are loaded from CDN
-define([ "dojo_basedeclare", "dijitDialog" ], function(declare, Dialog){
+define([ "dojo/_base/declare", "dijit/Dialog" ], function(declare, Dialog){
 //  declare our custom class
    return dojo.declare(dijit.Dialog, {
      title: "Hi"
